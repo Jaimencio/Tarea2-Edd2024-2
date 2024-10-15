64d09dc71145d6064d7fd23259388b707cebb424
Jaime Tomas Arias Jerez <jaime.arias@lds-pc35.inf.santiago.usm.cl>
2024-10-15 17:15:36 -0300

    PDF mal escrito xd

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <fstream>
 //#MA10
+using namespace std;
 struct Pelicula {
     string nombre;
     string director;
     float rating;
-}
+};
 
 class Director {
 private :
@@ -22,12 +23,11 @@ private :
     size_t size; // longitud lista
     string nombre_director;
 public:
-    void agregar_pelicula ( Peliculas * pelicula ); // agrega pelicula al final de la lista
-    enlazada
+    void agregar_pelicula ( Pelicula * pelicula ); // agrega pelicula al final de la lista enlazada
     void ordenar () ; // ordena la lista
     void calcular_rating_promedio ();
     void mostrar_peliculas ();
-}
+};
 
 class Arboles {
 private :
@@ -46,22 +46,23 @@ public :
     Arboles () ; // constructor
     ~ Arboles () ; // destructor
     void insertar_pelicula ( Pelicula * pelicula );
-    void copiar_arbol () // hace copia de arbol 1 en arbol 2 ordenado respecto de rating
+    void copiar_arbol (); // hace copia de arbol 1 en arbol 2 ordenado respecto de rating
     Director * buscar_director ( string director ); // retorna arreglo de peliculas
     Pelicula * buscar_pelicula ( string pelicula ); // retorna peliculas
-    3
-    void mejores_directores ( int n); // Muestra por pantalla los mejores n directores .
-    Enumerando de 1 a n.
-    void peores_directores ( int n ); // Muestra por pantalla los peores n directores .
-    Enumerando desde m ( cantidad de directores ) hasta m-n.
-}
-int main{
+    void mejores_directores ( int n); // Muestra por pantalla los mejores n directores. Enumerando de 1 a n.
+    void peores_directores ( int n ); // Muestra por pantalla los peores n directores.Enumerando desde m ( cantidad de directores ) hasta m-n.
+};
+int main(){
 
-    ifstream file;    
+    ifstream file;
+    string cant_pelis;
+
     file.open("Peliculas.txt");
     if (!file.is_open()){
         cerr << "No se pudo abrir el archivo" << endl;
         return 1;
     }
+    getline (file, cant_pelis); //obtener el valor de cantidad de lineas
+    int max_pelis = stoi(cant_pelis); 
     return 0;
 } 
